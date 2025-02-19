487f360a0fd94af915cb9429bcaec857348f90a6
Coder & AccoTax <codernaccotax@gmail.com>
2025-02-19 21:13:01 +0530

    first commit

---
+++ instructor/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Welcome Instructor</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item">
+          <a href="select_subjects.php" class="nav-link" aria-disabled="true">Insert Questions</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+
+<?php
+require_once "config.php";
+$stmt = $pdo->prepare('select * from users inner join user_types on users.user_type_id=user_types.id where users.email=?');
+$stmt->execute([$_POST['user_id']]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+   
+$return_array=[];
+if ($user && $_POST['password']=== $user['user_password']) {
+    $return_array['id'] = $user['id'];
+    $return_array['name'] = $user['email'];
+    $return_array['user_type_id'] = $user['user_type_id'];
+    $return_array['user_type_name'] = $user['user_type_name'];
+    $return_array['success'] = true;
+    
+   
+} else {
+    $return_array['success'] = false;
+}
+echo json_encode($return_array); 
